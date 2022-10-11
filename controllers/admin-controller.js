6ed38da3f92d87633f21abb8244a211ee3908b2a
yaahsin <brillanteoboe@gmail.com>
2022-10-11 14:57:35 +0800

    feat: get teachers' detail

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,33 @@
+const { user, course, role, user_role, enrollment } = require('../models')
+const { Op } = require("sequelize")
+
 // create, edit, viewAll, view, delete
 const adminController = {
+  getTeachers: async (req, res) => {
+    // 自組sub queries: role_id > user_id > find users detail
+    const roleId = await role.findAll({ attributes: ['id'], where: { name: "teacher" }, raw: true })
+    // user_id alias id to fit next query column_name: [ {id: 1}, {id: 2}... ]
+    const userId = await user_role.findAll({ attributes: [['user_id', 'id']], where: { roleId: roleId[0].id }, raw: true })
+    const teachers = await user.findAll({
+      attributes: { exclude: ['password'] },
+      where: {
+        [Op.or]: userId
+      }, raw: true
+    })
+
+    if (teachers.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        message: "Teachers not found"
+      })
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      teachers
+    })
+  },
+  getStudents: async (req, res) => {
+  }
 }
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
-// router.get('/courses', adminController.getCourses)
+
+
+router.get('/teachers', adminController.getTeachers)
+
 router.use('/', (req, res) => res.redirect('/'))
 
 module.exports = router
