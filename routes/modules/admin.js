0ffdacab01d81a9bda3997f1b099289804d96144
yaahsin <brillanteoboe@gmail.com>
2022-10-19 20:35:24 +0800

    fix: RESTful API

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,7 +5,7 @@ const { authenticated } = require('../../middleware/auth')
 const adminController = require('../../controllers/admin-controller')
 
 router.put('/enrollments/courses/:id', authenticated, adminController.editScores)
-router.get('/courses/open', authenticated, adminController.openCourses)
+router.get('/courses/openings', authenticated, adminController.openCourses)
 router.get('/courses/enrollment', authenticated, adminController.enrolledCourses)
 router.get('/courses/:id', adminController.getCourse)
 router.put('/courses/:id', authenticated, adminController.editCourse)
@@ -14,11 +14,10 @@ router.delete('/courses/:id', authenticated, adminController.deleteCourse)
 router.get('/teachers', authenticated, adminController.getTeachers)
 router.get('/students', authenticated, adminController.getStudents)
 router.get('/users/:id', authenticated, adminController.getUser)
-router.post('/user', authenticated, adminController.editUser)
+router.put('/user', authenticated, adminController.editUser)
 
 router.get('/courses', adminController.getCourses)
 router.post('/courses', authenticated, adminController.NewCourse)
-
 
 router.use('/', (req, res) => res.redirect('/'))
 
